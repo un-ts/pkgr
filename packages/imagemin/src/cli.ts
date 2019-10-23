25bd46f75cf5063ed3418ae11ece5d007ce17f60
JounQin <admin@1stg.me>
2019-10-23 12:20:55 +0800

    feat(imagemin): add glob support for input filenames

--- packages/imagemin/src/cli.ts
+++ packages/imagemin/src/cli.ts
@@ -1,7 +1,25 @@
 #!/usr/bin/env node
+import isGlob from 'is-glob'
+import glob from 'tiny-glob'
+
 import imagemin from '.'
 
-Promise.all(process.argv.slice(2).map(imagemin)).catch(e => {
-  console.error(e)
-  process.exitCode = 1
-})
+process.argv
+  .slice(2)
+  .reduce(
+    (files, file) =>
+      isGlob(file)
+        ? Promise.all([
+            files,
+            glob(file, {
+              filesOnly: true,
+            }),
+          ]).then(([files, matched]) => files.concat(matched))
+        : files.then(files => files.concat(file)),
+    Promise.resolve<string[]>([]),
+  )
+  .then(files => Promise.all(files.map(imagemin)))
+  .catch(e => {
+    console.error(e)
+    process.exitCode = 1
+  })
