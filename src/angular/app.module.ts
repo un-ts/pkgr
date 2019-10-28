3d75714e0fdd364ed12e988d043c622d01e9eb94
JounQin <admin@1stg.me>
2019-10-28 21:37:20 +0800

    feat(webpack): add seamless angular support :tada: (#35)

---
+++ src/angular/app.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+
+import { AppComponent } from './app.component'
+
+@NgModule({
+  imports: [BrowserModule, CommonModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class AppModule {}
