b4dfe1cb6f7b153be7d20bdb5934abfa0c1fabd1
JounQin <admin@1stg.me>
2025-06-01 09:39:11 +0800

    chore: bump all (dev)Dependencies and fix related usage issues

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -8,8 +8,11 @@ export interface CjsRequire extends NodeJS.Require {
   <T>(id: string): T
 }
 
-export const cjsRequire: CjsRequire =
-  typeof require === 'undefined' ? createRequire(import.meta.url) : require
+const importMetaUrl = import.meta.url
+
+export const cjsRequire: CjsRequire = importMetaUrl
+  ? createRequire(importMetaUrl)
+  : require
 
 export const EVAL_FILENAMES = new Set(['[eval]', '[worker eval]'])
 
