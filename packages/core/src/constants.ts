539c650506ae79e821fef8f9451c0602a08644fe
JounQin <admin@1stg.me>
2025-07-19 15:10:00 +0800

    fix: cleanup constants (#423)

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -2,19 +2,12 @@
 
 import { createRequire } from 'node:module'
 
-export const CWD = process.cwd()
-
 export interface CjsRequire extends NodeJS.Require {
   <T>(id: string): T
 }
 
-const importMetaUrl = import.meta.url
-
-export const cjsRequire: CjsRequire = importMetaUrl
-  ? createRequire(importMetaUrl)
-  : require
-
-export const EVAL_FILENAMES = new Set(['[eval]', '[worker eval]'])
+export const cjsRequire: CjsRequire =
+  typeof require === 'function' ? require : createRequire(import.meta.url)
 
 // eslint-disable-next-line sonarjs/deprecation
 export const EXTENSIONS = ['.ts', '.tsx', ...Object.keys(cjsRequire.extensions)]
